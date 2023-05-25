52dcffc94c1669990033380a8493105e9c14b6a3
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-05-26 01:30:43 +0400

    fix/add token

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-
 Route::controller(EmailVerificationController::class)->group(function () {
 	Route::post('email/{token}', 'verifyUser')->name('email.verify');
+});
+Route::controller(AuthController::class)->group(function () {
+	Route::post('register', 'register')->name('register');
+	Route::post('verify-email', 'sendVerificationEmail')->name('verify_email.send');
 });
