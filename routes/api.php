89e2e6fdc65b8937958a508db35452102e788c7a
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-05-25 20:32:30 +0400

    feat/add css fixer

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,5 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/confirm-email/{user}', [AuthController::class, 'confirmEmail'])->name('confirm.email');
