870877c90f1394899cce0e9f6c4087ba1f8da5ef
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-05-28 01:24:30 +0400

    wip/user login

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
 	Route::post('register', 'register')->name('register');
+	Route::post('login', 'login')->name('login');
 });
