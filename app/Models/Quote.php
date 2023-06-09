40c23af0a5495bb435f6c105381fa8dd43b87842
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-06-09 23:27:35 +0400

    feat/add new quote

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -2,15 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
-	protected $fillable = [
-		'body_en',
-		'body_ka',
-	];
-
 	use HasFactory;
+
+	protected $fillable = ['user_id', 'body', 'movie_id', 'thumbnail'];
+
+	public function movie()
+	{
+		return $this->belongsTo(Movie::class);
+	}
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	protected function body(): Attribute
+	{
+		return Attribute::make(
+			get: fn ($value) => json_decode($value, true),
+			set: fn ($value) => json_encode($value),
+		);
+	}
 }
