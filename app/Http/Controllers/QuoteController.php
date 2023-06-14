b65049cf700e20f386d194f290ead42fa130bba5
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-06-15 00:09:07 +0400

    feat/add movie logic

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -8,28 +8,34 @@ use Illuminate\Http\JsonResponse;
 
 class QuoteController extends Controller
 {
+	public function index(): JsonResponse
+	{
+		$quotes = Quote::latest()->with(['movie', 'user'])->paginate(2);
+
+		return response()->json($quotes);
+	}
+
 	public function store(AddQuotesRequest $request): JsonResponse
-{
-    if (!auth()->check()) {
-        return response()->json('Unauthorized', 401);
-    }
+	{
+		if (!auth()->check()) {
+			return response()->json('Unauthorized', 401);
+		}
 
-    $attributes = [
-        'body' => [
-            'en' => $request['body_en'],
-            'ka' => $request['body_ka'],
-        ],
-        'movie_id' => $request['movie_id'],
-        'thumbnail' => $request->file('thumbnail')->store('thumbnails'),
-        'user_id' => auth()->user()->id,
-    ];
+		$attributes = [
+			'body' => [
+				'en' => $request['body_en'],
+				'ka' => $request['body_ka'],
+			],
+			'movie_id'  => $request['movie_id'],
+			'thumbnail'    => $request['thumbnail'] = $request->file('thumbnail')->store('thumbnails', 'public'),
+			'user_id'   => auth()->user()->id,
+		];
 
-    $quote = Quote::create($attributes);
+		$quote = Quote::create($attributes);
+		if (!$quote) {
+			return response()->json('Something went wrong', 422);
+		}
 
-    if (!$quote) {
-        return response()->json('Something went wrong', 422);
-    }
-
-    return response()->json($quote, 200);
-}
+		return response()->json($quote, 200);
+	}
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,14 +4,57 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Movie extends Model
 {
 	use HasFactory;
 
+	protected $fillable = [
+		'name',
+		'user_id',
+		'genre',
+		'director',
+		'description',
+		'release_date',
+		'thumbnail',
+		'budget',
+	];
 
 	public function quotes()
 	{
 		return $this->hasMany(Quote::class);
+	}
+
+	protected function name(): Attribute
+	{
+		return Attribute::make(
+			get: fn ($value) => json_decode($value, true),
+			set: fn ($value) => json_encode($value),
+		);
+	}
+
+	protected function genre(): Attribute
+	{
+		return Attribute::make(
+			get: fn ($value) => json_decode($value, true),
+			set: fn ($value) => json_encode($value),
+		);
+	}
+
+	protected function director(): Attribute
+	{
+		return Attribute::make(
+			get: fn ($value) => json_decode($value, true),
+			set: fn ($value) => json_encode($value),
+		);
+	}
+
+	protected function description(): Attribute
+	{
+		return Attribute::make(
+			get: fn ($value) => json_decode($value, true),
+			set: fn ($value) => json_encode($value),
+		);
 	}
 }
