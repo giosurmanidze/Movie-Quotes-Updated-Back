e79f25f9286b3a01252fa1c30bb18a308873b478
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-06-15 00:09:07 +0400

    feat/add movie logic

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddMoviesRequest;
+use App\Models\Movie;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function store(AddMoviesRequest $request): JsonResponse
+	{
+		$movie = Movie::create([
+			'name' => [
+				'en' => $request['name_en'],
+				'ka' => $request['name_ka'],
+			],
+			'genre' => json_encode($request['genre']),
+
+			'director' => [
+				'en' => $request['director_en'],
+				'ka' => $request['director_ka'],
+			],
+			'description' => [
+				'en' => $request['description_en'],
+				'ka' => $request['description_ka'],
+			],
+			'user_id'      => auth()->user()->id,
+			'budget'       => $request['budget'],
+			'release_date' => $request['release_date'],
+			'thumbnail'    => $request['thumbnail'] = $request->file('thumbnail')->store('thumbnails', 'public'),
+		]);
+
+		if (!$movie)
+		{
+			return response()->json('Something went wrong', 422);
+		}
+
+		return response()->json($movie->load('quotes'), 200);
+	}
+
+	public function index(): JsonResponse
+	{
+		$userMovies = Movie::where('user_id', auth()->user()->id)->latest()->get();
+
+		$subset = $userMovies->map(function ($movie) {
+			return $movie->only(['id', 'name', 'thumbnail', 'release_date', 'quotes']);
+		});
+
+		return response()->json(
+			$subset
+		);
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GoogleLoginController;
 use App\Http\Controllers\PasswordResetController;
 use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'getUser']);
@@ -22,8 +23,14 @@ Route::controller(PasswordResetController::class)->group(function () {
 
 Route::controller(QuoteController::class)->group(function () {
 	Route::post('quotes', 'store')->name('quotes.store');
+	Route::get('quotes', 'index')->name('quotes.index');
 });
 
+Route::controller(MovieController::class)->group(function () {
+	Route::post('movies', 'store')->name('movies.store');
+	Route::get('movies', 'index')->name('movies.index');
+
+});
 Route::middleware(['web'])->group(function () {
 	Route::get('/auth/google/redirect', [GoogleLoginController::class, 'redirect']);
 	Route::get('/auth/google/callback', [GoogleLoginController::class, 'callback']);
