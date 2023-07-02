a0684e3952d06ec794c1ba7ebb1b1d9bc992192a
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-02 21:21:10 +0400

    feat/comment notification

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+
+class NotificationController extends Controller
+{
+    public function index()
+	{
+		return response()->json(Notification::where('to', auth()->user()->id)->with('sender')->orderBy('created_at', 'desc')->get());
+	}
+}
