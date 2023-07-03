ab1b98f377034482a84306497b0b0ecd5daff21f
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-03 12:02:12 +0400

    refactor/remove unnesessary code

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,22 +8,14 @@ class NotificationController extends Controller
 {
 	public function index()
 	{
-		return response()->json(Notification::where('to', auth()->user()->id)->with('sender')->orderBy('created_at', 'desc')->get());
+		return response()->json(Notification::where('to', auth()->id())->with('sender')->orderBy('created_at', 'desc')->get());
 	}
 
 	public function markAsRead()
 	{
-		$notification = Notification::where('read', '=', false);
-
-		$notification->update(['read' => true]);
-
-		$allNotifications = Notification::latest()->where('to', auth()->user()->id)->with('sender')->get();
+		Notification::where('read', '=', false)->update(['read' => true]);
+		$allNotifications = Notification::latest()->where('to', auth()->id())->with('sender')->get();
 
 		return response()->json($allNotifications, 200);
-	}
-
-	public function unread()
-	{
-		return response()->json(Notification::where('to', '=', auth()->user()->id)->where('read', '=', false)->count());
 	}
 }
