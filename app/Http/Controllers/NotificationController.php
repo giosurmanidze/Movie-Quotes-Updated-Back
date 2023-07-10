d096ecdb9b04fbdd68922bf46b31c2203f535f3c
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-07-10 19:46:13 +0400

    fix/possibility to make single notification seened

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Notification;
+use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
@@ -11,11 +12,24 @@ class NotificationController extends Controller
 		return response()->json(Notification::where('to', auth()->id())->with('sender')->orderBy('created_at', 'desc')->get());
 	}
 
-	public function markAsRead()
+	public function markAsRead(Request $request)
 	{
-		Notification::where('read', '=', false)->update(['read' => true]);
-		$allNotifications = Notification::latest()->where('to', auth()->id())->with('sender')->get();
-
+		if ($request->has('id')) {
+			$notification = Notification::find($request->id);
+			if (!$notification) {
+				return response()->json(['message' => 'Notification not found'], 404);
+			}
+			$notification->read = true;
+			$notification->save();
+		} else {
+			Notification::where('to', auth()->id())->update(['read' => true]);
+		}
+		$allNotifications = Notification::latest()
+			->where('to', auth()->id())
+			->with('sender')
+			->get();
+	
 		return response()->json($allNotifications, 200);
 	}
+	
 }
