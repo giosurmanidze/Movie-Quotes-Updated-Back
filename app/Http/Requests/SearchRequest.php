1d70187c34842c22c6f17cf71179acb95b091661
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-12 21:07:51 +0400

    fix/add searching without symbol  and move this logic in scopeFilter

--- app/Http/Requests/SearchRequest.php
+++ app/Http/Requests/SearchRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class SearchQuoteRequest extends FormRequest
+class SearchRequest extends FormRequest
 {
 	/**
 	 * Get the validation rules that apply to the request.
