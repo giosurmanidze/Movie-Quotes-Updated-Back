4d2bc2f72693ad1d20f9bf6466a45031e2395a7f
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-13 21:30:17 +0400

    fix/return json response in login method and update comment logic

--- database/migrations/2023_06_20_164714_create_comments_table.php
+++ database/migrations/2023_06_20_164714_create_comments_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
 		Schema::create('comments', function (Blueprint $table) {
 			$table->id();
 			$table->text('body');
-			$table->text('username');
 			$table->foreignId('quote_id')->constrained()->cascadeOnDelete();
 			$table->foreignId('user_id')->constrained()->cascadeOnDelete();
 			$table->timestamps();
